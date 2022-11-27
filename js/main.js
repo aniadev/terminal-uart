cb4db38436b1f5653b1cd2a0cd7a44c8899df94e
Hai Pham <haiham1999.90@gmail.com>
2022-11-27 15:29:37 +0700

    update for mr.an sheer hust: terminal viewer

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ new Vue({
 	data() {
 		return {
 			isAutoScroll: true,
-			isShowTimestamp: false,
+			isShowTimestamp: true,
 			// isConnected: false,
 			rxData: '',
 			txData: '',
@@ -29,11 +29,77 @@ new Vue({
 			],
 			newLineSelected: 'NO_LINE_ENDING',
 			baudrateOptions: [300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200],
-			baudrate: 9600,
+			baudrate: 115200,
 			port: null,
 			reader: null,
-			isReadingSerial: false,
+			onReadingSerial: false,
 			outputPanel: null,
+
+			allRecordData: [],
+			currentRecordData: {
+				data: [
+					{
+						id: 1,
+						label: 'Tk',
+						value: '30.22',
+					},
+					{
+						id: 2,
+						label: 'To',
+						value: '31.22',
+					},
+					{
+						id: 3,
+						label: 'Tgn',
+						value: '33.22',
+					},
+					{
+						id: 4,
+						label: 'Tbl1',
+						value: '34.22',
+					},
+					{
+						id: 5,
+						label: 'Tbl2',
+						value: '35.22',
+					},
+				],
+				timestamp: Date.now(),
+			},
+			chart: null,
+			chartLabels: [],
+			chartData: [
+				{
+					label: 'T1',
+					data: [],
+					borderWidth: 1,
+					borderColor: '#e74c3c',
+				},
+				{
+					label: 'T2',
+					data: [],
+					borderWidth: 1,
+					borderColor: '#3498db',
+				},
+				{
+					label: 'T3',
+					data: [],
+					borderWidth: 1,
+					borderColor: '#16a085',
+				},
+				{
+					label: 'T4',
+					data: [],
+					borderWidth: 1,
+					borderColor: '#34495e',
+				},
+				{
+					label: 'T5',
+					data: [],
+					borderWidth: 1,
+					borderColor: '#7f8c8d',
+				},
+			],
 		}
 	},
 	methods: {
@@ -50,6 +116,13 @@ new Vue({
 		async connectSerial() {
 			try {
 				console.log('connect')
+				setTimeout(() => {
+					this.onReadingSerial = true
+					console.log('START_READING_SERIAL')
+					this.rxObjData = {}
+					this.rxData = ''
+				}, 2000)
+
 				const filter = {usbVendorId: 0x2341}
 				this.port = await navigator.serial.requestPort({filters: [filter]})
 				await this.port.open({baudRate: this.baudrate})
@@ -65,14 +138,16 @@ new Vue({
 						}
 						dnc = new TextDecoder('utf-8')
 						str = dnc.decode(value)
-						this.rxData += str
-						if (str.includes('\n')) {
-							this.rxObjData = {
-								timestamp: Date.now(),
-								value: this.rxData,
+						if (this.onReadingSerial) {
+							this.rxData += str
+							if (str.includes('\n')) {
+								this.rxObjData = {
+									timestamp: Date.now(),
+									value: this.rxData,
+								}
+								this.onReadline()
+								this.rxData = ''
 							}
-							this.onReadline()
-							this.rxData = ''
 						}
 					}
 				} catch (error) {
@@ -87,6 +162,7 @@ new Vue({
 			}
 		},
 		async disconnectSerial() {
+			this.onReadingSerial = false
 			this.reader.releaseLock()
 			await this.port.close()
 			this.port = null
@@ -102,10 +178,36 @@ new Vue({
 			if (this.isAutoScroll) {
 				this.scrollToBottom()
 			}
+			this.mapRecordData()
+			this.mapChartData()
 		},
 		scrollToBottom() {
 			this.outputPanel.scrollTop = this.outputPanel.scrollHeight
 		},
+		mapRecordData() {
+			const arrData = this.rxData.trimEnd().split(',')
+			this.currentRecordData.data[0].value = arrData[0] ? arrData[0] : '-'
+			this.currentRecordData.data[1].value = arrData[1] ? arrData[1] : '-'
+			this.currentRecordData.data[2].value = arrData[2] ? arrData[2] : '-'
+			this.currentRecordData.data[3].value = arrData[3] ? arrData[3] : '-'
+			this.currentRecordData.data[4].value = arrData[4] ? arrData[4] : '-'
+			this.currentRecordData.timestamp = new Date().toString()
+
+			this.allRecordData.push(this.currentRecordData)
+		},
+		mapChartData() {
+			const arrData = this.rxData.trimEnd().split(',')
+			const timestamp = new Date().toLocaleTimeString('vi-VN')
+			this.addData(timestamp, arrData)
+		},
+		addData(label, arrData) {
+			this.chart.data.labels.push(label);
+			this.chart.data.datasets.forEach((dataset, index) => {
+				dataset.data.push(arrData[index] || null);
+			});
+			this.chart.update();
+		}
+		
 	},
 	computed: {
 		portInfo() {
@@ -124,6 +226,23 @@ new Vue({
 	mounted() {
 		this.outputPanel = document.getElementById('output-panel')
 		this.scrollToBottom()
+
+		const ctx = document.getElementById('myChart')
+		this.chart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: this.chartLabels,
+				datasets: this.chartData
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true,
+					},
+				},
+			},
+		})
+		console.log(this.chart)
 	},
 	destroy() {},
 })
